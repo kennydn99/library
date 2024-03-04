43474372ea3ef14b8a857541bc158f5e391f68fa
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-03-04 15:26:26 -0800

    Add example books and work on creatign Book object with user input

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//Array to store book objects
-const myLibrary = [];
 
 //Book constructor
 function Book(title, author, pages, read) {
@@ -12,24 +10,41 @@ function Book(title, author, pages, read) {
     }
 }
 
+const book1 = new Book('Dune', 'Frank Herbert', 896, false);
+const book2 = new Book("Can't Hurt Me", 'David Goggins', 364, false);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+console.log(theHobbit.info());
+
+//Array to store book objects
+const myLibrary = [book1, book2, theHobbit];
+
+
 const dialog = document.querySelector('#addBookDialog');
 const addBookBtn = document.querySelector('.add-book-btn');
 const cancelBtn = document.querySelector('#closeDialog');
+
 addBookBtn.addEventListener('click', () => {
     dialog.show();
 });
+
 cancelBtn.addEventListener('click', () => {
     dialog.close();
 });
 
 // take user’s input and store the new book objects into an array
 function addBookToLibrary() {
+    const titleField = document.querySelector('#title');
+    const authorField = document.querySelector('#author');
+    const pageNumField = document.querySelector('#pages');
+    const haveReadCheck = document.querySelector('#read-check');
+    const newBook = new Book(titleField.value, authorField.value, pageNumField.value, haveReadCheck.value);
+    console.log(newBook.info);
 }
 
 //Loop through array and display each book on page
 function displayBook() {
-    
+    for (const book of myLibrary) {
+        console.log(book);
+    }
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-console.log(theHobbit.info());
