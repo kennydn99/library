2f3b7be55887ef108142267f7023ba233e4abca4
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-03-06 13:53:16 -0800

    Update display when user adds a new book

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const book1 = new Book('Dune', 'Frank Herbert', 896, false);
 const book2 = new Book("Can't Hurt Me", 'David Goggins', 364, false);
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 
-//Array to store book objects
-const myLibrary = [book1, book2, theHobbit];
+//Array to store book objects - book1, book2, theHobbit
+const myLibrary = [];
 
 const dialog = document.querySelector('#addBookDialog');
 const addNewBookBtn = document.querySelector('.add-book-btn');
@@ -39,57 +39,68 @@ function addBookToLibrary() {
 
     const newBook = new Book(bookTitle, bookAuthor, bookPageNum, bookRead);
     console.log(newBook);
+    newBook.hasBeenDisplayed = false;
     myLibrary.push(newBook);
 
     bookForm.reset();
     dialog.close();
 }
 
+//create bookcard function
+function createBookCard(book) {
+    const bookContainer = document.querySelector('.books-container');
+
+    const bookCard = document.createElement("div");
+    bookCard.classList.add('card');
+    
+    const bookCardTitle = document.createElement('h1');
+    bookCardTitle.textContent = book.title;
+    
+    const bookCardAuthor = document.createElement('h3');
+    bookCardAuthor.textContent = 'by ' + book.author;
+    
+    const bookCardPages = document.createElement('span');
+    bookCardPages.textContent = book.pages + " pages";
+    
+    const bookCardRead = document.createElement('button');
+    bookCardRead.classList.add('book-card-btn');
+    if (book.read) {
+        bookCardRead.textContent = "Read";
+        bookCardRead.classList.add('read');
+    } else {
+        bookCardRead.textContent = "Not Read";
+        bookCardRead.classList.add('not-read');
+    }
+    
+    const removeBookBtn = document.createElement('button');
+    removeBookBtn.classList.add('book-card-btn', 'remove-btn');
+    removeBookBtn.textContent = 'Remove Book';
+    
+    bookCard.appendChild(bookCardTitle);
+    bookCard.appendChild(bookCardAuthor);
+    bookCard.appendChild(bookCardPages);
+    bookCard.appendChild(bookCardRead);
+    bookCard.appendChild(removeBookBtn);
+    bookContainer.appendChild(bookCard);
+}
+
+
 const addBookBtn = document.querySelector('#add-book-to-library-btn');
 addBookBtn.addEventListener('click', (e) => {
     e.preventDefault();
     addBookToLibrary();
+    displayBook();
 });
 
 //Loop through array and display each book on page
 function displayBook() {
-    const bookContainer = document.querySelector('.books-container');
-    for (const book of myLibrary) {
-        console.log(book);
+    for (const myBook of myLibrary) {
+        console.log(myBook);
         //create book 'card'
-        const bookCard = document.createElement("div");
-        bookCard.classList.add('card');
-        
-        const bookCardTitle = document.createElement('h1');
-        bookCardTitle.textContent = book.title;
-
-        const bookCardAuthor = document.createElement('h3');
-        bookCardAuthor.textContent = 'by ' + book.author;
-
-        const bookCardPages = document.createElement('span');
-        bookCardPages.textContent = book.pages + " pages";
-
-        const bookCardRead = document.createElement('button');
-        bookCardRead.classList.add('book-card-btn');
-        if (book.read) {
-            bookCardRead.textContent = "Read";
-            bookCardRead.classList.add('read');
-        } else {
-            bookCardRead.textContent = "Not Read";
-            bookCardRead.classList.add('not-read');
+        if (!myBook.hasBeenDisplayed) {
+            createBookCard(myBook);
+            myBook.hasBeenDisplayed = true;
         }
         
-        const removeBookBtn = document.createElement('button');
-        removeBookBtn.classList.add('book-card-btn', 'remove-btn');
-        removeBookBtn.textContent = 'Remove Book';
-
-        bookCard.appendChild(bookCardTitle);
-        bookCard.appendChild(bookCardAuthor);
-        bookCard.appendChild(bookCardPages);
-        bookCard.appendChild(bookCardRead);
-        bookCard.appendChild(removeBookBtn);
-        bookContainer.appendChild(bookCard);
-
     }
 }
-
